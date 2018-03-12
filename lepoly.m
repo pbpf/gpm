4ac193ccafc6bfbeed320ef86ff96d3de1c783c7
pbpf <kevin2059@163.com>
2018-03-12 09:41:31 +0800

    add 添加注释

--- lepoly.m
+++ lepoly.m
@@ -1,8 +1,9 @@
 function [dy,y]=lepoly(N,x)
-%% Legendre多项式P_N 和其导数的值 N>=1
+%% Legendre多项式P_N 和其导数的值 N>=1 
 %  利用Legendre多项式的性质：L_0(x)=1,L_1(x)=x,L_2(x)=3/2*x^2-1/2
 %                          (n+1)L_n+1(x)=(2n+1)x*L_n(x)-n*L_n-1(x),n>=1
 %                          L_N(1)==1;l_N(-1)=(-1)^N;
+%  connor 2018/3/12 edit
 L0=1;
 L1=x;
 dL0=0;
